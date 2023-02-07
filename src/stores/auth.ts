0e0a89cc9b9873fd950eac72f11d3b449feaa304
Michał Olejniczak <blind.x6@gmail.com>
2023-02-07 16:51:24 +0100

    Add auth store

---
+++ src/stores/auth.ts
@@ -0,0 +1,47 @@
+import { defineStore } from 'pinia'
+
+import Credentials from '@/types/credentials'
+
+import api from '@/lib/api'
+
+interface User {
+    email: string,
+    username?: string,
+}
+
+export const useAuthStore = defineStore('auth', {
+    state: () => {
+        return {
+            token: sessionStorage.getItem('token') || null,
+            currentUser: null as User | null,
+        }
+    },
+    getters: {
+        isLoggedIn: (state) => !!state.token,
+    },
+    actions: {
+        logout() {
+            sessionStorage.removeItem('token')
+            location.reload()
+        },
+        async retrieveToken(credentials: Credentials) {
+            try {
+                const { token, user } = await api.post('login', credentials)
+                sessionStorage.setItem('token', token)
+                this.token = token
+                this.currentUser = user
+            } catch (error) {
+                return error
+            }
+        },
+        async fetchCurrentUser() {
+            if (!this.token) return
+
+            const user = await api.get(`users/me`)
+            this.currentUser = user
+        },
+    }
+
+})
+
+
