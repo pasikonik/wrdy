a4de433d0b57863f9f3e3135e6f26604b8db7a4b
Michał Olejniczak <blind.x6@gmail.com>
2023-02-07 16:51:09 +0100

    Create routing

---
+++ src/router.ts
@@ -0,0 +1,65 @@
+import { createRouter, RouteRecordRaw, createWebHashHistory } from 'vue-router'
+
+import { useAuthStore } from '@/stores/auth'
+
+import Home from './views/Home.vue'
+import Login from '@/views/Login.vue'
+import SignUp from '@/views/SignUp.vue'
+import Lists from '@/views/Lists.vue'
+import Import from '@/views/Import.vue'
+import Stats from '@/views/Stats.vue'
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+    },
+    {
+        path: '/sign-up',
+        name: 'sign-up',
+        component: SignUp,
+    },
+    {
+        path: '/lists',
+        name: 'lists',
+        component: Lists,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/import',
+        name: 'import',
+        component: Import,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/stats',
+        name: 'stats',
+        component: Stats,
+        meta: { requiresAuth: true }
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+router.beforeEach((to, _from, next) => {   
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        const auth = useAuthStore()
+        if (auth.isLoggedIn) {
+            return next()
+        }
+        next('/login')
+    } else {
+        next()
+    } 
+})
+
+export default router
