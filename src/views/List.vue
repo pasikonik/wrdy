fe6fab9575c6602dbf29b122df2a059478b3ea5e
Michał Olejniczak <blind.x6@gmail.com>
2023-02-13 16:08:16 +0100

    Showing list name in main view

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,57 +1,62 @@
 <script setup lang="ts">
-import { ref } from 'vue' 
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 import { useListStore } from '@/stores/list'
 
 const newWordOrigin = ref('')
 const newWordTranslation = ref('')
 
-
+const route = useRoute()
 const store = useListStore()
 
-store.fetchLists()
-
-const list = null
+const list = store.getListById(route.params.id)
 
 function addWord() {
-    console.log('adding word');
+  console.log('adding word')
 }
-
 </script>
 
-<template>
-    <h2>List jedna</h2>
+<template v-if="list">
+  {{ route.params.id }}
+  <h2>{{ list.name }}</h2>
 
-    <div class="content" v-if="list">
-        <h1 class="d-flex align-center" v-if="list">
-            <v-avatar rounded color="blue" size="36" class="mr-3">
-                <v-icon dark> mdi-format-list-bulleted </v-icon>
-            </v-avatar>
-            {{ list.name }}
-        </h1>
-        <div>words: {{ list.words.length }}</div>
-    
-        <v-form>
-            <v-container>
-                <v-row>
-                    <v-col cols="12" md="5">
-                        <v-text-field v-model="newWordOrigin" label="Origin" required></v-text-field>
-                    </v-col>
-    
-                    <v-col cols="12" md="5">
-                        <v-text-field v-model="newWordTranslation" label="Translation" required></v-text-field>
-                    </v-col>
-    
-                    <v-col cols="12" md="2" class="pt-6">
-                        <v-btn block color="primary" @click="addWord"> Add </v-btn>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-form>
-    
-        <div v-for="word in list.words" :key="word.id">
-            {{ word.origin }} - {{ word.translation }} - {{ word.proficiency }}
-        </div>
+  <div v-if="list" class="content">
+    <h1 v-if="list" class="d-flex align-center">
+      <v-avatar rounded color="blue" size="36" class="mr-3">
+        <v-icon dark> mdi-format-list-bulleted </v-icon>
+      </v-avatar>
+      {{ list.name }}
+    </h1>
+    <!-- <div>words: {{ list.words.length }}</div> -->
+
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="5">
+            <v-text-field
+              v-model="newWordOrigin"
+              label="Origin"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="5">
+            <v-text-field
+              v-model="newWordTranslation"
+              label="Translation"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="2" class="pt-6">
+            <v-btn block color="primary" @click="addWord"> Add </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+
+    <div v-for="word in list.words" :key="word.id">
+      {{ word.origin }} - {{ word.translation }} - {{ word.proficiency }}
     </div>
+  </div>
 </template>
-
-
