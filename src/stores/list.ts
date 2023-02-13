90abd086ba9d66b39465d52135dd730b8a2c24cb
Michał Olejniczak <blind.x6@gmail.com>
2023-02-13 16:07:47 +0100

    Add login functionality to remove list

--- src/stores/list.ts
+++ src/stores/list.ts
@@ -2,25 +2,32 @@ import api from '@/lib/api'
 import { defineStore } from 'pinia'
 
 interface List {
-    id: number,
-    name: string,
+  id: string
+  name: string
 }
 
-
 export const useListStore = defineStore('list', {
-    state: () => {
-        return {
-            lists: [] as List[]
-        }
-    },
-    actions: {
-        async createList(newListName: string) {
-            const newList = api.post('lists', { name: newListName })
-            this.lists.push(newList)
-        },
-        async fetchLists() {
-            const result = await api.get('lists')
-            this.lists = result
-        }
+  state: () => {
+    return {
+      lists: [] as List[],
     }
+  },
+  getters: {
+    getListById: (state) => {
+      return (listId: string) => state.lists.find((list) => list.id == listId)
+    },
+  },
+  actions: {
+    async createList(newListName: string) {
+      const newList = await api.post('lists', { name: newListName })
+      this.lists.push(newList)
+    },
+    async fetchLists() {
+      const result = await api.get('lists')
+      this.lists = result
+    },
+    async destroyList(listId) {
+      await api.delete(`lists/${listId}`)
+    },
+  },
 })
