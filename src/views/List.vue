37de281541771e944ddae6511e3c16cf65c4c7ed
Michał Olejniczak <blind.x6@gmail.com>
2023-02-20 14:31:47 +0100

    Continue List view improvements

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,32 +1,49 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { onMounted, computed, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import { useListStore } from '@/stores/list'
+import List from '@/types/list'
 
 const newWordOrigin = ref('')
 const newWordTranslation = ref('')
 
 const route = useRoute()
+const router = useRouter()
 const store = useListStore()
+const paramId = route.params.id?.[0]
 
-const list = store.getListById(route.params.id)
+store.fetchList(paramId)
+let list = store.getListById(paramId)
+
+// onMounted(() => {
+
+// })
 
 function addWord() {
   console.log('adding word')
 }
+
+const deleteList = async () => {
+  if (!list) return
+  await store.deleteList(list.id)
+  router.push('/lists')
+}
 </script>
 
-<template v-if="list">
-  {{ route.params.id }}
-  <h2>{{ list.name }}</h2>
+<template>
+  <main v-if="list" class="listContainer">
+    <v-btn
+      class="deleteButton"
+      icon="mdi-trash-can-outline"
+      color="primary"
+      @click="deleteList"
+    />
 
-  <div v-if="list" class="content">
-    <h1 v-if="list" class="d-flex align-center">
-      <v-avatar rounded color="blue" size="36" class="mr-3">
-        <v-icon dark> mdi-format-list-bulleted </v-icon>
-      </v-avatar>
+    <h1 class="d-flex align-center">
       {{ list.name }}
     </h1>
+
+    <div>id: {{ list.id }}</div>
     <!-- <div>words: {{ list.words.length }}</div> -->
 
     <v-form>
@@ -49,14 +66,29 @@ function addWord() {
           </v-col>
 
           <v-col cols="12" md="2" class="pt-6">
-            <v-btn block color="primary" @click="addWord"> Add </v-btn>
+            <v-btn color="primary" variant="text" @click="addWord"> Add </v-btn>
           </v-col>
         </v-row>
       </v-container>
     </v-form>
 
-    <div v-for="word in list.words" :key="word.id">
-      {{ word.origin }} - {{ word.translation }} - {{ word.proficiency }}
+    <div>
+      <div v-for="word in list.words" :key="word.id">
+        {{ word.origin }} - {{ word.translation }} - {{ word.proficiency }}
+      </div>
     </div>
-  </div>
+  </main>
 </template>
+
+<style scoped>
+.listContainer {
+  text-align: left;
+  position: relative;
+}
+
+.deleteButton {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+</style>
