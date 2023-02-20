9506b54e99f55bbfe3cbd4435932dc3fca5ea62e
Michał Olejniczak <blind.x6@gmail.com>
2023-02-20 14:27:33 +0100

    Add fetching data before mount app

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import vuetify from '@/plugins/vuetify'
+import { useListStore } from '@/stores/list'
+import { useAuthStore } from '@/stores/auth'
 
 const pinia = createPinia().use(createORM())
 
@@ -14,4 +16,15 @@ app.use(pinia)
 app.use(router)
 app.use(vuetify)
 
-app.mount('#app')
+const listStore = useListStore()
+const authStore = useAuthStore()
+
+if (authStore.isLoggedIn) {
+  Promise.all([authStore.fetchCurrentUser(), listStore.fetchLists()]).then(
+    () => {
+      app.mount('#app')
+    }
+  )
+} else {
+  app.mount('#app')
+}
