d3db7f9b02dd679f6525f293226c636a523f3d28
Michał Olejniczak <blind.x6@gmail.com>
2023-02-22 21:30:20 +0100

    Create placeholder component for no lists view

---
+++ src/components/NoList.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="d-flex flex-column align-center">
+    <h2 class="text-h2 ma-15">You don't have any list</h2>
+
+    <h3 clas="text-h3">Create new one to add some words</h3>
+  </main>
+</template>
