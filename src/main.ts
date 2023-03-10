dd45fb35e296ae910de53d39ac8057eabde004e8
Michał Olejniczak <blind.x6@gmail.com>
2023-03-10 21:21:42 +0100

    Use top level await in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
 import router from './router'
 import vuetify from '@/plugins/vuetify'
-import { useListStore } from '@/stores/list'
 import { useAuthStore } from '@/stores/auth'
 
 const pinia = createPinia()
@@ -15,15 +14,11 @@ app.use(pinia)
 app.use(router)
 app.use(vuetify)
 
-const listStore = useListStore()
 const authStore = useAuthStore()
 
 if (authStore.isLoggedIn) {
-  Promise.all([authStore.fetchCurrentUser(), listStore.fetchLists()]).then(
-    () => {
-      app.mount('#app')
-    }
-  )
+  await authStore.fetchCurrentUser()
+  app.mount('#app')
 } else {
   app.mount('#app')
 }
