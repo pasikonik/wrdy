6169500717badd06897f32cb63b148b93ee5e19b
Michał Olejniczak <blind.x6@gmail.com>
2023-03-10 21:17:18 +0100

    Refactor SignUp and Login components

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,20 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref, reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 
 const router = useRouter()
 const store = useAuthStore()
 
-const email = ref<string>('')
-const password = ref<string>('')
-const showPassword = ref<boolean>(false)
-
-const credentials = computed(() => {
-  return {
-    email: email.value,
-    password: password.value,
-  }
+const credentials = reactive({
+  email: '',
+  password: '',
 })
+const showPassword = ref<boolean>(false)
 
 async function login() {
   try {
-    await store.retrieveToken(credentials.value)
+    await store.retrieveToken(credentials)
     router.push('/lists')
   } catch (error) {
     console.log(error)
@@ -30,17 +25,17 @@ async function login() {
 <template>
   <v-card width="300" class="pa-2 mx-auto mt-16" variant="outlined">
     <v-card-title class="text-center">
-      <h2 class="display-1">Login</h2>
+      <h2 class="text-h4 my-3">Login</h2>
     </v-card-title>
     <v-card-text>
       <v-form>
         <v-text-field
-          v-model="email"
+          v-model="credentials.email"
           label="Email"
           prepend-icon="mdi-account-circle"
         />
         <v-text-field
-          v-model="password"
+          v-model="credentials.password"
           :type="showPassword ? 'text' : 'password'"
           name="password"
           label="Password"
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,55 +1,51 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import { useRouter } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 
+const router = useRouter()
 const store = useUserStore()
 
-const email = ref<string>('')
-const password = ref<string>('')
+const userParams = reactive({
+  email: '',
+  password: '',
+})
 const showPassword = ref<boolean>(false)
 
-function register() {
-  store.create({ 
-    email: email.value, 
-    password: password.value,
-  })
+async function register() {
+  await store.create(userParams)
+  router.push('/')
 }
 </script>
 
 <template>
   <v-card width="300" class="pa-2 mx-auto mt-16" variant="outlined">
     <v-card-title class="text-center">
-      <h2 class="display-1">Sign Up</h2>
+      <h2 class="text-h4 my-3">Sign Up</h2>
     </v-card-title>
     <v-card-text>
       <v-form>
         <v-text-field
-          v-model="email"
+          v-model="userParams.email"
           label="Email"
           prepend-icon="mdi-account-circle"
         />
         <v-text-field
-          v-model="password"
+          v-model="userParams.password"
           :type="showPassword ? 'text' : 'password'"
           name="password"
           label="Password"
           prepend-icon="mdi-lock"
           :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
           @click:append="showPassword = !showPassword"
+          @keyup.enter="register"
         />
       </v-form>
     </v-card-text>
     <v-card-actions>
-      <v-btn 
-        color="success" 
-        variant="outlined"
-        @click="register"
-        block 
-      >
+      <v-btn color="success" variant="outlined" block @click="register">
         Sign Up
       </v-btn>
     </v-card-actions>
   </v-card>
 </template>
-
-<style></style>
