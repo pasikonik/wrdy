a01200a9d2682eb29d868a11649420f6082a0c40
Michał Olejniczak <blind.x6@gmail.com>
2023-04-04 17:05:28 +0200

    Handle sass variables

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.scss'
 import App from './App.vue'
 import router from './router'
 import vuetify from '@/plugins/vuetify'
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "./src/styles/index.scss" as *;',
+      },
+    },
+  },
 })
