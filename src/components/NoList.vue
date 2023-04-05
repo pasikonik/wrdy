30d5a089f99578f7ba73105b772b50cfd2e22896
Michał Olejniczak <blind.x6@gmail.com>
2023-04-05 14:22:05 +0200

    Improve header size

--- src/components/NoList.vue
+++ src/components/NoList.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="d-flex flex-column align-center">
-    <h2 class="text-h2 ma-15">You don't have any list</h2>
+    <h1 class="text-h3 ma-15">You don't have any list</h1>
 
     <h3 clas="text-h3">Create new one to add some words</h3>
   </main>
