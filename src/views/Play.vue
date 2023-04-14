b8bd9d7dbda4b1ac976d179e3a33dc218d5ed9a0
Michał Olejniczak <blind.x6@gmail.com>
2023-04-14 15:53:36 +0200

    Create Play view

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useWordStore } from '@/stores/word'
+import { storeToRefs } from 'pinia'
+import LearnWords from '@/components/LearnWords.vue'
 
-<template>Play</template>
+defineProps<{ listId: number }>()
 
-<style scoped lang="scss"></style>
+const isLoading = ref(false)
+
+const wordStore = useWordStore()
+const { pickWordsToLearn } = storeToRefs(wordStore)
+
+// const words = pickWordsToLearn.value(props.listId)
+</script>
+
+<template>
+  <setion>
+    <div v-if="isLoading" class="d-flex flex-column align-center">
+      <h1 class="text-h3 mb-10 text-grey-darken-1">Let's play a game</h1>
+      <v-progress-circular
+        :size="70"
+        :width="7"
+        color="orange"
+        indeterminate
+      ></v-progress-circular>
+    </div>
+    <div v-else>
+      <learn-words :words="pickWordsToLearn(listId)" />
+    </div>
+  </setion>
+</template>
