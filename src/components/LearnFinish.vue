5c08e3e4e6428f7938e46e370f5c75aa0dd01207
Michał Olejniczak <blind.x6@gmail.com>
2023-04-26 20:59:50 +0200

    Prepare initial view for finished play

---
+++ src/components/LearnFinish.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  efficiency: number
+  listId: number
+}>()
+</script>
+
+<template>
+  <h2 class="text-h4 text-center">Good job!</h2>
+
+  Efficiency: {{ efficiency }}%
+
+  <span>Improved proficiency for X words</span>
+
+  <div class="d-flex justify-space-between">
+    <v-btn
+      variant="outlined"
+      size="large"
+      :to="{ name: 'list', params: { id: listId } }"
+    >
+      I'm done
+    </v-btn>
+    <v-btn
+      color="green"
+      variant="flat"
+      size="large"
+      :to="{ name: 'play', params: { id: listId } }"
+    >
+      Next play
+    </v-btn>
+  </div>
+</template>
